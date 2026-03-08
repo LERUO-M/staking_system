bee3b9a406aed0db55aa076886f03af006a7687e
lemotlhcjc025 <lemotlhcjc025@student.wethinkcode.co.za>
2026-03-08 22:26:40 +0200

    Added a simple test to check if a user can stake.

--- test/TokenStaking.test.js
+++ test/TokenStaking.test.js
@@ -38,8 +38,28 @@ describe("Running tests for the staking token contract", function() {
         expect(await tokenstaked.rewardRate()).to.equal(ethers.parseUnits("0.1", 18));
     });
 
-    it("", async function () {
+    it("Should not be able to stake 0 LMT tokens", async function () {
+        const { tokenstaked, user1 } = await loadFixture(deployContract);
+        await expect(tokenstaked.connect(user1).stake(0)).to.be.revertedWith("Cannot stake 0");
+    });
+
+    it("Should be able to stake LMT tokens > 0", async function () {
+        const { tokenstaked, stakingToken, owner, user1 } = await loadFixture(deployContract);
         
+        // Owner transfers 100 tokens to User1 so they have something to stake
+        const amountToStake = ethers.parseUnits("100", 18);
+        await stakingToken.connect(owner).transfer(user1.address, amountToStake);
+
+
+        // User1 must Approve the staking contract to spend their tokens
+        await stakingToken.connect(user1).approve(await tokenstaked.getAddress(), amountToStake);
+
+        // User1 stakes the tokens
+        await expect(tokenstaked.connect(user1).stake(amountToStake)).not.to.be.reverted;
+
+        // 4. Check the Staked Balance (not balanceOf)
+        const balance = await tokenstaked.stakedBalance(user1.address);
+        expect(balance).to.equal(amountToStake);
     });
 
 
